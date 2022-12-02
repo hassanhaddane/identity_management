fb657f11219db8523cafec32a951b42f7baca1f0
Hassan HADDANE <hassanhaddane33s@gmail.com>
2022-12-02 11:27:18 +0100

    Adding Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppMaterialModule } from './app-material.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {MatLegacyListModule} from "@angular/material/legacy-list";
+import {RouterLink} from "@angular/router";
+import { LdapListComponent } from './ldap-list/ldap-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LdapListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppMaterialModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatLegacyListModule,
+    RouterLink
   ],
   providers: [],
   bootstrap: [AppComponent]
