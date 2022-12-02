5c84b1fd7681cfa7e73ad1cf79441adb9104dbfb
Hassan HADDANE <hassanhaddane33s@gmail.com>
2022-12-02 12:29:51 +0100

    Adding ldap list component

---
+++ src/app/ldap-list/ldap-list.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import {MatTableDataSource} from "@angular/material/table";
+import {UserLdap} from "../model/user-ldap";
+import {LDAP_USERS} from "../model/ldap-mock-data";
+
+@Component({
+  selector: 'app-ldap-list',
+  templateUrl: './ldap-list.component.html',
+  styleUrls: ['./ldap-list.component.scss']
+})
+export class LdapListComponent implements OnInit {
+  displayedColumns: string[] = ['nomComplet', 'mail', 'employeNumero'];
+  dataSource = new MatTableDataSource<UserLdap>([]);
+
+  @ViewChild(MatPaginator, {static: true}) paginator:MatPaginator;
+   constructor() { }
+
+  ngOnInit(): void {
+     this.dataSource.paginator = this.paginator;
+     this.getUsers();
+  }
+
+  private getUsers(): void {
+     this.dataSource.data = LDAP_USERS;
+  }
+}
