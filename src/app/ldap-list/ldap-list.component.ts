caf022f6eec53ce352c56af3977cd4907f1fd1c7
Hassan HADDANE <hassanhaddane33s@gmail.com>
2023-01-18 08:37:21 +0100

    Update ldap list component (Adding : AfterViewInit )

--- src/app/ldap-list/ldap-list.component.ts
+++ src/app/ldap-list/ldap-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import {MatTableDataSource} from "@angular/material/table";
 import {UserLdap} from "../model/user-ldap";
@@ -9,7 +9,7 @@ import {LDAP_USERS} from "../model/ldap-mock-data";
   templateUrl: './ldap-list.component.html',
   styleUrls: ['./ldap-list.component.scss']
 })
-export class LdapListComponent implements OnInit {
+export class LdapListComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = ['nomComplet', 'mail', 'employeNumero'];
   dataSource = new MatTableDataSource<UserLdap>([]);
 
@@ -17,11 +17,18 @@ export class LdapListComponent implements OnInit {
    constructor() { }
 
   ngOnInit(): void {
+     console.log('Value on ngOnInit():');
      this.dataSource.paginator = this.paginator;
+     console.log("Mat Paginator", this.paginator);
      this.getUsers();
   }
 
   private getUsers(): void {
      this.dataSource.data = LDAP_USERS;
+  }
+
+  ngAfterViewInit(): void {
+     console.log('Values on ngAfterViewInit():');
+     console.log("Mat Paginator:", this.paginator);
   }
 }
